d36274d6315a73f15421b64019d7b4b1b5a3a71f
GBATTIZOCCO <gabriele.battizocco@outlook.it>
2026-02-06 11:12:54 +0100

    Update static.py

--- static/static.py
+++ static/static.py
@@ -3,4 +3,4 @@ She took offense at his comment.
 Yes, it is unfair to the people who paid off their student loans to cancel everyone's debt now.
 On the other hand, I feel like I just have to do it for my sanity.
 """
-"""
+
